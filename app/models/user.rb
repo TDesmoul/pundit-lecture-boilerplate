90d17cf40fce25f5a789a2c93e2f242d3caf1dad
tdesmoul <thomas.desmoulins@gmail.com>
2024-02-29 10:35:13 +0100

    boilerplate for pundit lecture

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+  def generate_identicon
+    Identicon.data_url_for self.email, 64, [255, 255, 255]
+  end
 end
